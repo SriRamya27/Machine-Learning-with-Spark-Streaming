cd09b08676e1183ff97adfc8a0ac9e487a445616
adityas-31 <86249074+adityas-31@users.noreply.github.com>
2021-11-20 02:34:29 +0530

    added hashingtf and idf (preprocessing)

--- streaming_test.py
+++ streaming_test.py
@@ -9,6 +9,16 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.ml.feature import RegexTokenizer
 import pyspark.sql.types as tp
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, CountVectorizer
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml import Pipeline, pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml import Pipeline
+
 import re
 import json
 
@@ -19,24 +29,40 @@ alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',  'm
                 , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',\
                 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-
+"""
 def remove_url(line):
+    # if len(line) != 0:
+    #     for i in line:
+    #         try:
+    #             words = i[2].split(' ')
+    #             print(words)
+    #         except:
+    #             print("")
+    #             pass
     # line = re.sub(r"http\S+" , "" , line)
     # line = re.sub('  +', '' , line)
     # line = re.sub('\n+', '' , line)
     # line = line.split('\n')
+    print(line)
     return line
-
+"""
 
 def display(rdd):
 
     sent = rdd.collect()
+    # print(sent)
+    # sent = remove_url(sent)
     if len(sent) > 0:
         try:
-            df = spark.createDataFrame(json.loads(sent[0]).values() , schema = ["sentiment" , "tweet"])
-            tokenizer = RegexTokenizer(inputCol="tweet", outputCol="SentimentWords" ,  pattern= '\\W')
-            tokenized = tokenizer.transform(df)
-            tokenized.show(truncate=False)
+            df = spark.createDataFrame(json.loads(sent[0]).values() , schema = ["Sentiment" , "Tweet"])
+            tokenizer = RegexTokenizer(inputCol="Tweet", outputCol="SentimentWords" ,  pattern= '\\W')
+            hashtf = HashingTF(numFeatures=2**16 , inputCol="SentimentWords" , outputCol="tf")
+            idf = IDF(inputCol="tf" , outputCol="features" , minDocFreq= 5)
+            label_stringIdx = StringIndexer(inputCol="Sentiment" , outputCol="label") #by default, 0 (from dataset) is mapped to 1, and 4 to 0
+            pipeline = Pipeline(stages=[tokenizer , hashtf , idf , label_stringIdx])
+            pipelineFit = pipeline.fit(df)
+            train_df = pipelineFit.transform(df)
+            train_df.show(truncate=False , n = 5)
         except:
             print("mistake")
             pass
@@ -49,7 +75,7 @@ if __name__ == "__main__":
     sql_context=SQLContext(sc)
     tweets = ssc.socketTextStream("localhost" , 6100)   
 
-    words = tweets.flatMap(lambda line : remove_url(line).split('\n'))
+    words = tweets.flatMap(lambda line : line.split('\n'))
     # words = tweets.flatMap(lambda line : re.sub(r"http\S+" , "" , line).split('\n'))
     words.foreachRDD(display)
 
