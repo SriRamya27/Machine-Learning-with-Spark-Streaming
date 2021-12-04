0483714cd6c939cdfc4d68c8097eb6849a0b7b8f
Sri Ramya Priya <70714711+SriRamya27@users.noreply.github.com>
2021-12-04 23:59:57 +0530

    Add files via upload

---
+++ classification.py
@@ -0,0 +1,45 @@
+'''
+python3 stream.py -f sentiment -b 10000 
+$SPARK_HOME/bin/spark-submit stream_preprocess.py > output.txt
+'''
+
+import numpy as np
+import pickle
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.cluster import MiniBatchKMeans, KMeans
+
+def naiveBayes(x,y):	
+	# load the model from disk
+	nb_loaded_model = pickle.load(open('nb_finalized_model.sav', 'rb'))
+	nb_loaded_model.partial_fit(x,y, np.unique(y))
+	print("in nb")
+	print(nb_loaded_model.predict(x[2]),y[2])
+#naiveBayes()	
+	
+def perceptron(x,y):	
+	# load the model from disk
+	perceptron_loaded_model = pickle.load(open('perceptron_finalized_model.sav', 'rb'))
+	perceptron_loaded_model.partial_fit(x,y, np.unique(y))
+	print("in perceptron")
+	print(perceptron_loaded_model.predict(x[2]),y[2])
+#perceptron()
+
+def sdg(x,y):	
+	# load the model from disk
+	sgd_loaded_model = pickle.load(open('sgd_finalized_model.sav', 'rb'))
+	sgd_loaded_model.partial_fit(x,y, np.unique(y))
+	print("in sgd")
+	print(sgd_loaded_model.predict(x[2]),y[2])
+#sgd()	
+
+
+def kmeans(x,y):	
+	# load the cluster from disk
+	kmeans_loaded_cluster = pickle.load(open('kmeans_finalized_cluster.sav', 'rb'))
+	kmeans_loaded_cluster.partial_fit(x)
+	print("in kmeans")
+	print(kmeans_loaded_cluster.predict(x[2]),y[2])
+#kmeans()
+
