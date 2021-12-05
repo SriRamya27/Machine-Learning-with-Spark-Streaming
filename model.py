61b46c169ef2aac911e21f02b5768f29fb2a9307
Sri Ramya Priya <70714711+SriRamya27@users.noreply.github.com>
2021-12-05 23:40:17 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -9,25 +9,24 @@ nb_model = BernoulliNB()
 filename = 'nb_finalized_model.sav'
 with open(filename, 'wb') as f:
     pickle.dump(nb_model, f)
- 
     
-perceptron_model =  Perceptron(tol=1e-3, random_state=0)
+perceptron_model =  Perceptron(alpha=0.1, tol=1e-3, random_state=0)
 # save the model to disk
 filename = 'perceptron_finalized_model.sav'
 with open(filename, 'wb') as f:
     pickle.dump(perceptron_model, f)
   
     
-sgd_model = SGDClassifier(max_iter=1000, tol=0.01)
+sgd_model = SGDClassifier(alpha=0.01, max_iter=1000, tol=0.01, learning_rate='optimal')
 # save the model to disk
 filename = 'sgd_finalized_model.sav'
 with open(filename, 'wb') as f:
     pickle.dump(sgd_model, f)
 
 
-#from sklearn.cluster import MiniBatchKMeans
-kmeans = MiniBatchKMeans(n_clusters=2)
 
+kmeans = MiniBatchKMeans(n_clusters=2, max_iter=1000, tol=1.0, max_no_improvement=20, reassignment_ratio=0.0001)
+# save the model to disk
 filename = 'kmeans_finalized_cluster.sav'
 with open(filename, 'wb') as f:
     pickle.dump(kmeans, f)
