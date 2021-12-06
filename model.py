e1f002544b2bc4b896c554eaec68e57b801cea39
shweta-patki <62463995+shweta-patki@users.noreply.github.com>
2021-12-06 22:29:16 +0530

    Cleaning

--- model.py
+++ model.py
@@ -1,35 +1,28 @@
+#This file creates models for Naive Bayes, Perceptron, Stochastic Gradient Descent, and K-Means classifiers.
+#It imports models from sklearn, tunes them with the hyperparameters that perform best, and saves them as a .sav file (in binary format).
+
 import pickle
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.cluster import MiniBatchKMeans
-#alpha=0.1-->better performed than without hyperparameter
+
 nb_model = BernoulliNB(alpha=0.1)
-# save the model to disk
 filename = 'nb_finalized_model.sav'
 with open(filename, 'wb') as f:
     pickle.dump(nb_model, f)
-#alpha=0.1-->better performed than alpha=0.001
+
 perceptron_model =  Perceptron(alpha=0.01,tol=1e-3, random_state=0)
-# save the model to disk
 filename = 'perceptron_finalized_model.sav'
 with open(filename, 'wb') as f:
     pickle.dump(perceptron_model, f)
-  
-#alpha=0.1-->better performed than hyperparameter=0.001
+ 
 sgd_model = SGDClassifier(alpha=0.01,max_iter=1000, tol=0.01,learning_rate='optimal')
-# save the model to disk
 filename = 'sgd_finalized_model.sav'
 with open(filename, 'wb') as f:
     pickle.dump(sgd_model, f)
 
-
-#from sklearn.cluster import MiniBatchKMeans
-#reassignment_ratio=>0.001 is better and max_no of improvement is more
 kmeans = MiniBatchKMeans(n_clusters=2,max_iter=1000,tol=1.0,max_no_improvement=20,reassignment_ratio=0.001)
-
 filename = 'kmeans_finalized_cluster.sav'
 with open(filename, 'wb') as f:
     pickle.dump(kmeans, f)
-
-
